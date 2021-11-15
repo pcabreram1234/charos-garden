39485dd522f137dcf342eb9def954361b38826ff
pcabreram <pcabreram1234@gmail.com>
2021-11-15 00:04:54 -0400

    Experimentando asignado la logico del modal en otro archivo

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import useFetchProduct from "../hooks/useFetchProduct";
+import MainPlantCollage from "../components/MainPlantCollage";
+import Modal2 from "./Modal2";
 import ZoomIcon from "../assets/images/zoom.png";
+import "../styles/ProductContainer.css";
+import * as modalFuntions from "../utils/modalFuntions";
 
-const ProductContainer = ({ id }) => {
+const ProductContainer = (props) => {
+  console.log(modalFuntions);
+  const { id } = props;
   const fetch = useFetchProduct(id);
   let srcEl = "";
+  let title = "";
 
   for (const el of fetch) {
     srcEl = el.src;
+    title = el.name;
   }
   return (
     <div className="ProductContainer">
-      <h1>Ejemplo</h1>
-      <div className="ProductContainer-image">
-        <img src={ZoomIcon} alt="" />
-      </div>
+      <Modal2
+        title={title}
+        visible={modalFuntions.default.showModal}
+        onOk={modalFuntions.default.handleOk}
+        onCancel={modalFuntions.default.handleCancel}
+        width={"100%"}
+        centered={true}
+        destroyOnClose
+        footer={null}
+        src={srcEl}
+      />
       <div className="ProductContainer-button">
+        <img
+          src={ZoomIcon}
+          alt=""
+          onClick={modalFuntions.default.showModal()}
+        />
+      </div>
+      <div className="ProductContainer-image">
+        <h1>{title}</h1>
         <img src={srcEl} alt="" />
+      </div>
+      <div className="Product-collage-Container">
+        <MainPlantCollage src={srcEl} />
       </div>
     </div>
   );
---
+++ src/utils/modalFuntions.js
@@ -0,0 +1,13 @@
+const showModal = () => {
+  true ? false : true;
+};
+
+const handleOk = () => {
+  return false;
+};
+
+const handleCancel = () => {
+  return false;
+};
+
+export default { showModal, handleOk, handleCancel };
