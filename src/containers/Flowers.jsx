abbb50c22d78965a47947dcd3bae26994770251e
Phillilp Leonardo Cabrera Medrano <pcabreram@Phillilps-MacBook-Pro.local>
2022-02-13 17:20:31 -0400

    Pagina de flowers

--- src/containers/Flowers.jsx
+++ src/containers/Flowers.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import FlowersJSon from "../utils/flowersBd.json";
 import "../styles/Flowers.css";
 
-const Flowers = (props) => {
+const Flowers = () => {
+  const [flowerInfo, setFlowerInfo] = useState([]);
+  const [flowerSrc, setFlowerSrc] = useState([]);
+  const [title, setTitle] = useState([]);
+
+  function getFlowerInfo(info, src, title) {
+    setFlowerInfo(info);
+    setFlowerSrc(src);
+    setTitle(title);
+  }
+
   const items = FlowersJSon.Flowers.map((el) => {
     return (
-      <div className="item_Container" key={el.id}>
+      <div
+        className="item_Container"
+        key={el.id}
+        onClick={() => {
+          getFlowerInfo(el.detail, el.src, el.name);
+        }}
+      >
         <img src={el.src} alt="" />
       </div>
     );
@@ -13,8 +29,16 @@ const Flowers = (props) => {
 
   return (
     <div className="Flowers__Container">
-      <div className="Flower_Main__Container"></div>
+      <div className="CurrentFlower_Container">
+        <div className="CurrentFlowe-info">
+          <h1>{title}</h1>
+          <p>{flowerInfo}</p>
+        </div>
 
+        <div className="CurrentFlower--img">
+          <img src={flowerSrc} alt="" />
+        </div>
+      </div>
       <div className="Flowers__Album">{items}</div>
     </div>
   );
