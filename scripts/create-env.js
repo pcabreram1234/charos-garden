3ebd11e9a26f573c4f950dbfa49ccc1bfb2bb15f
Phillip Cabrera <pcabreram1234@gmail.com>
2022-03-06 01:05:01 -0400

    Deployment test

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 
 fs.writeFileSync(
   "./.env",
-  `API_FLOWERS=${process.env.API_FLOWERS}\n API_PLANTS=${API_PLANTS}\n API_GIFTS=${API_GIFTS}\n `
+  `API_FLOWERS=${process.env.API_FLOWERS}\n API_PLANTS=${process.env.API_PLANTS}\n API_GIFTS=${process.env.API_GIFTS}\n `
 );
